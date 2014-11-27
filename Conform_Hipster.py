b35bae17367bd93290fc4eb2d3360ec9f5f892ba
Maor Bernstein <maor.bernstein@students.olin.edu>
2014-11-27 01:25:43 -0800

    Added delay

--- Conform_Hipster.py
+++ Conform_Hipster.py
@@ -7,11 +7,11 @@ from __future__ import print_function, division
 
 import csv
 import math
-import numpy
+import numpy as np
 import sys
 
 import matplotlib
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
 import thinkbayes2
 import thinkplot
@@ -21,13 +21,16 @@ import thinkplot
 class Hipster(thinkbayes2.Suite):
   """doc strings"""
   
-  def __init__(self, microPopulation, N, label=None):
+  def __init__(self, N, label=None):
     
-    self.microPopulation = microPopulation
     
     pmf = thinkbayes2.Pmf()
+    mu_p = .6
+    std_p = .1
     for i in range(N):
-      pmf.Set(i/(N-1),1)
+      o = i/(N-1.0)
+      pmf.Set(o,thinkbayes2.EvalNormalPdf(o,mu_p,std_p) )
+      
     pmf.Normalize()
     
     thinkbayes2.Suite.__init__(self, pmf, label = label)
@@ -40,19 +43,22 @@ class Hipster(thinkbayes2.Suite):
     """
     opinion = hypo
     (mean,std) = data
-    like = thinkbayes2.EvalNormalPdf(hypo,.9-mean,std)
+    like = thinkbayes2.EvalNormalPdf(hypo,1 - mean,std)
     return like
     
     
 class Conformist(thinkbayes2.Suite):
   """doc strings"""
   
-  def __init__(self, microPopulation, N, label=None):
-    self.microPopulation = microPopulation
+  def __init__(self, N, label=None):
   
     pmf = thinkbayes2.Pmf()
+    mu_p = .6
+    std_p = .1
     for i in range(N):
-      pmf.Set(i/(N-1),1)
+      o = i/(N-1.0)
+      pmf.Set(o, thinkbayes2.EvalNormalPdf(o,mu_p,std_p) )
+      
     pmf.Normalize()
     
     thinkbayes2.Suite.__init__(self,pmf,label = label)
@@ -71,39 +77,54 @@ class Conformist(thinkbayes2.Suite):
     
     
 def main():
-  N = 100 #Number of iterations
-  hipsterMicroPopulation = 100
-  conformistMicroPopulation = 500
-  hip = Hipster(hipsterMicroPopulation, 1000)
-  conf = Conformist(conformistMicroPopulation, 1000)
+  N = 18 #Number of iterations
+  hipfrac = 1
+  conffrac = 1 - hipfrac
+  hip = Hipster(1000)
+  conf = Conformist(1000)
+  hipmean = []
+  confmean = []
+  popmean = []
+  
   for i in range(N):
-    hipmean = hip.Mean()
+    hipmean.append(hip.Mean())
     hipvar = hip.Var()
-    confmean = conf.Mean()
+    confmean.append(conf.Mean())
     confvar = conf.Var()
-    hipPop = hip.microPopulation
-    confPop = conf.microPopulation
-    popmean = (hipmean*hipPop + confmean*confPop)/(hipPop + confPop)
-    popvar = (hipvar*hipPop + confvar*confPop)/(hipPop + confPop)
+    popmean.append(hipmean[i]*hipfrac + confmean[i]*conffrac)
+    popvar = hipvar*hipfrac + confvar*conffrac
     popstd = math.sqrt(popvar)
-    thinkplot.Pmf(hip,label = 'Hipsters')
-    thinkplot.show()
-    thinkplot.Pmf(conf,label = 'Conformists')
-    thinkplot.show()
 
-    hip.Update((popmean,popstd))
-    conf.Update((popmean,popstd))
+    hip.Update((popmean[i],popstd))
+    conf.Update((popmean[i],popstd))
+    
+  plt.plot(range(N),hipmean)
+  plt.plot(range(N),confmean)
+  plt.plot(range(N),popmean)
+  plt.show()
 
-def main2():
-  print('running main 2')
-  N = 2 #Number of iterations
-  hipsterMicroPopulation = 100
-  conformistMicroPopulation = 500
-  hip = Hipster(hipsterMicroPopulation, 1000)
-  conf = Conformist(conformistMicroPopulation, 1000)
-  print('starting thing plot')
-  thinkplot.Pmf(hip,label = 'Hipsters')
-  thinkplot.Pmf(conf,label = 'Conformists')
-  thinkplot.show()
-  print('finished')
-main()
+  
+
+def main_onlyhipsters():
+  N = 18 #Number of iterations
+  hipfrac = 1
+  hip = Hipster(1000)
+  hipmean = []
+
+  hipmean.append(hip.Mean())
+  hipvar = hip.Var()
+  hipstd = math.sqrt(hipvar)
+  hip.Update((hipmean[0],hipstd))
+    
+  for i in range(1,N):
+    hipmean.append(hip.Mean())
+    hipvar = hip.Var()
+    hipstd = math.sqrt(hipvar)
+    hip.Update((hipmean[i-1],hipstd))
+
+  plt.plot(range(N),hipmean)
+  plt.show()
+  
+
+if __name__ == "__main__":
+    main_onlyhipsters()
